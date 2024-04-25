880974b99edb7b078496b331b4db5062fd09334c
teamelitecoder <teamelitecoder@gmail.com>
2024-04-25 18:39:24 -0400

    Initial form

--- scouting-app.py
+++ scouting-app.py
@@ -3,5 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
+
+@app.route("/testing")
+def testhub():
+    return render_template("testing.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def registration():
+    return render_template("registration.html")
