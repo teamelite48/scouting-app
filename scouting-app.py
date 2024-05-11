d24b2c8c6ee13c55aa2826f07bb9d24a5e464cfc
Matthew Jones <14156163+mtjones330@users.noreply.github.com>
2024-05-11 09:59:42 -0400

    WIP: docker compose works with live reload

--- scouting-app.py
+++ scouting-app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import pickle
 import os
+from datetime import datetime
+import sys
 
 app = Flask(__name__)
 app.debug = True
@@ -63,6 +65,11 @@ def drivedashboard():
 
 @socketio.on('live reload')
 def handle_live_reload(message):
-    print(message)
+    log(message)
 
-socketio.run(app, host="0.0.0.0", allow_unsafe_werkzeug=True)
+def log(message):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(timestamp + ": " + message)
+    sys.stdout.flush()
+
+socketio.run(app, host="0.0.0.0", allow_unsafe_werkzeug=True, use_reloader=False)
