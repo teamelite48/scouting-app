f9d1c01ddd232a8743f3dbf2076b3a30bc6165dc
Team Elite Coder <122939378+teamelitecoder@users.noreply.github.com>
2024-12-18 19:57:20 -0500

    streamlined app flow

---
+++ data/forms.py
@@ -0,0 +1,16 @@
+from data import db
+
+
+collection = "2024_forms"
+
+def add(form):
+  return db.insert_one(collection, form)
+
+def getAll():
+  return db.find(collection)
+
+def get(id):
+  return db.find_by_id(collection, id)
+
+def update(id, form):
+  return db.update_one(collection, id, form)
--- scouting-app.py
+++ scouting-app.py
@@ -4,7 +4,8 @@ from flask import Flask, render_template, request, redirect
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_socketio import SocketIO
 from config import CONFIG
-from data import matches
+from data import forms
+from data import scouters
 from data import users
 from utils.log import log
 import datetime
@@ -25,35 +26,36 @@ login_manager.login_view = "login"
 
 def get_form_options():
     return {
+        "scouters": list(map(lambda scouter: scouter["name"], scouters.getAll())),
         "starting_position": [
-            { "key": "1", "value": "Not There" },
-            { "key": "2", "value": "Source" },
-            { "key": "3", "value": "Middle of Speakers"},
-            { "key": "4", "value": "Amp Side"}
+            "Not There",
+            "Source",
+            "Middle of Speaker",
+            "Amp Side"
         ],
         "left_start": [
-            { "key": "1", "value": "No" },
-            { "key": "2", "value": "Yes" },
-            { "key": "3", "value": "Attempted"}
+            "No",
+            "Yes",
+            "Attempted"
         ],
         "midline_collect": [
-            { "key": "1", "value": "No" },
-            { "key": "2", "value": "Yes" },
-            { "key": "3", "value": "Attempted"},
-            { "key": "4", "value": "Ready for Teleop"}
+            "No",
+            "Yes",
+            "Attempted",
+            "Ready for Teleop"
         ],
         "intake": [
-            { "key": "1", "value": "Does not pick up from the floor"},
-            { "key": "2", "value": "Over the Bumper"},
-            { "key": "3", "value": "Under the Bumper"}
+            "Does not pick up from the floor",
+            "Over the Bumper",
+            "Under the Bumper"
         ],
         "end_status": [
-            { "key": "1", "value": "Not Parked"},
-            { "key": "2", "value": "Parked"},
-            { "key": "3", "value": "Stage Attempted"},
-            { "key": "4", "value": "On Stage" },
-            { "key": "5", "value": "Hung with 1 Robot" },
-            { "key": "6", "value": "Hung with 2 Robots"}
+            "Not Parked",
+            "Parked",
+            "Stage Attempted",
+            "On Stage",
+            "Hung with 1 Robot",
+            "Hung with 2 Robots"
         ]
     }
 
@@ -85,8 +87,9 @@ def login():
         user.authenticated = True
         users.save(user)
         login_user(user)
-        next = flask.request.args.get("next")
-        return flask.redirect(next or '/')
+        # next = flask.request.args.get("next")
+        # return flask.redirect(next or '/scouting-dashboard')
+        return redirect('/scouting_dashboard')
 
     return render_template("/login.html")
 
@@ -107,7 +110,7 @@ def logout():
 @app.route("/")
 @login_required
 def home_page():
-  return render_template("home.html", user_data={})
+  return redirect("/scouting_dashboard")
 
 
 @app.route("/form/2024/new", methods=['GET'])
@@ -117,13 +120,13 @@ def new_2024_form():
 
 
     vm = {
-        "scouter_name": "",
-        "starting_position": 1,
+        "match_number": "",
+        "starting_position": "Not There",
         "midline_collect": 1,
         "midline_time": 0,
         "intake": 1,
-        "starthang_time": 0,
-        "stophang_time": 0,
+        "start_hang": 0,
+        "stop_hang": 0,
         "end_status": 1,
         "comments": "",
         "options": get_form_options()
@@ -139,8 +142,9 @@ def save_2024_form():
 
   form = request.form
   created_on = str(datetime.datetime.now())
-  matches.add({
-        "scouter_name": form.get("scouter_name"),
+  forms.add({
+        "scouter": form.get("scouter"),
+        "match_number": form.get("match_number"),
         "starting_position": form.get("starting_position"),
         "left_start": form.get("left_start"),
         "midline_collect": form.get("midline_collect"),
@@ -171,42 +175,43 @@ def save_2024_form():
         "updated_on": created_on
     })
 
-  return redirect("/completed_matches")
+  return redirect("/scouting_dashboard")
 
 @app.route("/form/2024/<id>", methods=['GET'])
 @login_required
 def load_2024_form(id):
 
-    match = matches.get(id)
+    form = forms.get(id)
 
     vm = {
-      "scouter_name": match.get("scouter_name"),
-      "starting_position": match.get("starting_position"),
-      "left_start": match.get("left_start"),
-      "midline_collect": match.get("midline_collect"),
-      "midline_time": match.get("midline_time"),
-      "auto_amp_score": match.get("auto_amp_score"),
-      "auto_amp_misses": match.get("auto_amp_misses"),
-      "auto_speaker_score": match.get("auto_speaker_score"),
-      "auto_speaker_misses": match.get("auto_speaker_misses"),
-      "source_pickup": match.get("source_pickup"),
-      "intake": match.get("intake"),
-      "under_stage": match.get("under_stage"),
-      "teleop_amp_score": match.get("teleop_amp_score"),
-      "teleop_amp_misses": match.get("teleop_amp_misses"),
-      "teleop_speaker_score": match.get("teleop_speaker_score"),
-      "teleop_speaker_misses": match.get("teleop_speaker_misses"),
-      "start_hang": match.get("start_hang"),
-      "stop_hang": match.get("stop_hang"),
-      "trap_score": match.get("trap_score"),
-      "end_status": match.get("end_status"),
-      "fell_over": match.get("fell_over"),
-      "stopped_working": match.get("stopped_working"),
-      "unstable_driving": match.get("unstable_driving"),
-      "turned_off": match.get("turned_off"),
-      "connection_issues": match.get("connection_issues"),
-      "good_defense": match.get("good_defense"),
-      "comments": match.get("comments"),
+      "scouter": form.get("scouter"),
+      "match_number": form.get("match_number"),
+      "starting_position": form.get("starting_position"),
+      "left_start": form.get("left_start"),
+      "midline_collect": form.get("midline_collect"),
+      "midline_time": form.get("midline_time"),
+      "auto_amp_score": form.get("auto_amp_score"),
+      "auto_amp_misses": form.get("auto_amp_misses"),
+      "auto_speaker_score": form.get("auto_speaker_score"),
+      "auto_speaker_misses": form.get("auto_speaker_misses"),
+      "source_pickup": form.get("source_pickup"),
+      "intake": form.get("intake"),
+      "under_stage": form.get("under_stage"),
+      "teleop_amp_score": form.get("teleop_amp_score"),
+      "teleop_amp_misses": form.get("teleop_amp_misses"),
+      "teleop_speaker_score": form.get("teleop_speaker_score"),
+      "teleop_speaker_misses": form.get("teleop_speaker_misses"),
+      "start_hang": form.get("start_hang"),
+      "stop_hang": form.get("stop_hang"),
+      "trap_score": form.get("trap_score"),
+      "end_status": form.get("end_status"),
+      "fell_over": form.get("fell_over"),
+      "stopped_working": form.get("stopped_working"),
+      "unstable_driving": form.get("unstable_driving"),
+      "turned_off": form.get("turned_off"),
+      "connection_issues": form.get("connection_issues"),
+      "good_defense": form.get("good_defense"),
+      "comments": form.get("comments"),
       "options": get_form_options()
     }
 
@@ -218,8 +223,9 @@ def update_2024_form(id):
 
   form = request.form
 
-  matches.update(id, {
-    "scouter_name": form.get("scouter_name"),
+  forms.update(id, {
+    "scouter": form.get("scouter"),
+    "match_number": form.get("match_number"),
     "starting_position": form.get("starting_position"),
     "left_start": form.get("left_start"),
     "midline_collect": form.get("midline_collect"),
@@ -249,16 +255,16 @@ def update_2024_form(id):
     "updated_on": str(datetime.datetime.now())
   })
 
-  return redirect("/completed_matches")
+  return redirect("/scouting_dashboard")
 
 
-@app.route("/completed_matches", methods=['GET'])
+@app.route("/scouting_dashboard", methods=['GET'])
 @login_required
-def completed_matches():
+def scouting_dashboard():
 
-    sorted_matches = sorted(matches.getAll(), key=itemgetter("created_on"), reverse=True)
+    sorted_forms = sorted(forms.getAll(), key=itemgetter("created_on"), reverse=True)
 
-    return render_template("completed_matches.html", matches=sorted_matches)
+    return render_template("scouting_dashboard.html", forms=sorted_forms)
 
 
 if CONFIG.PROD == False:
