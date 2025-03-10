6a2896b30f0fba984cd9bf743dc28d52d7f5bc0d
mrsqueakssss <90198673+mrsqueakssss@users.noreply.github.com>
2025-03-09 23:27:34 -0400

    Saturday bugfixes (#21)
    * fixed counter code not decreasing
    
    * added scouter name into forms, started super scouting form
    
    * finished super scouting form
    
    * fixed coral and algae intake dropdowns being switched. Also switched team select to be a text box temporarily until team database can be finished. fixed processor accuracy.
    
    * removed trap score as it is no longer needed
    
    * wip saving qual forms
    
    * wip qual table
    
    * updated components
    
    * made forms more pleasing to look at
    
    * allowed blank submissions to work
    
    * added previous commit to id post
    
    * made counters start on 0
    
    * added the ability to make text boxes required
    
    * fixed comma error in text_area component
    
    * fixed super scouting form not loading
    
    * Made the scouting dashboard header also function as a home button
    
    * added user D
    
    * Made the login page prettier looking
    
    * fixed oversight making counters always be 0, meaning counters need to still be made 0 by default
    
    * ironed out coral and algae intake
    
    * added scouter name to qual form
    
    * reverted quals to forms in qual new post
    
    * added climb speed datapoint
    
    * Rewrote end status options
    
    * Made starting position options less ambiguous
    
    * Labeled the end status options with real world identifiers
    
    * made counters start at 0
    
    * finished saving functionality for super scouting form, started pit form
    
    * fixed data panel being broken
    
    * Finished pit form
    
    * Moved Climb Speed Location to farther down the list

--- docker/mongo-init.js
+++ docker/mongo-init.js
@@ -3,6 +3,8 @@ print('🌱 Seeding MongoDB...');
 app = db.getSiblingDB("scouting_app");
 
 app.createCollection("2025_forms");
+app.createCollection("qual_forms");
+app.createCollection("pit_forms");
 app.createCollection("users");
 app.createCollection("teams");
 
@@ -10,8 +12,48 @@ forms = app.getCollection("2025_forms");
 
 forms.insertOne({
   "scouter_name": "Elite Scouter",
-  "age": 48,
+  "age": "Test",
   "created_on": "2025-01-01 00:34:01.721103"
+});
+
+quals = app.getCollection("qual_forms");
+
+quals.insertOne({
+  "team": "Test",
+  "match_number": "Test",
+  "scouter_name": "Test",
+  "comments": "Test",
+  "created_by": "Test",
+  "updated_by": "Test",
+  "created_on": "2025-01-01 00:34:01.721103",
+  "updated_on": "2025-01-01 00:34:01.721103",
+});
+
+pits = app.getCollection("pit_forms");
+
+pits.insertOne({
+  "scouter_name": "Test",
+  "team": "Test",
+  "base": "Test",
+  "dimensions": "Test",
+  "weight": "Test",
+  "gravity": "Test",
+  "algae_intake": "Test",
+  "coral_intake": "Test",
+  "L4": "Test",
+  "L3": "Test",
+  "L2": "Test",
+  "L1": "Test",
+  "auto_move": "Test",
+  "auto_score": "Test",
+  "auto_best_score": "Test",
+  "shallow": "Test",
+  "deep": "Test",
+  "comments": "Test",
+  "created_by": "Test",
+  "updated_by": "Test",
+  "created_on": "2025-01-01 00:34:01.721103",
+  "updated_on": "2025-01-01 00:34:01.721103"
 });
 
 users = app.getCollection("users");
@@ -33,6 +75,13 @@ users.insertOne({
 
 users.insertOne({
   "username": "C",
+  "password": "$2b$12$Lao7omDmMROC8wewoi9LmOrKsEJI/2Z75D802ILccBbhJmwIT3oxu", // team0048
+  "admin": false,
+  "authenticated": false
+});
+
+users.insertOne({
+  "username": "D",
   "password": "$2b$12$Lao7omDmMROC8wewoi9LmOrKsEJI/2Z75D802ILccBbhJmwIT3oxu", // team0048
   "admin": false,
   "authenticated": false
@@ -70,6 +119,5 @@ teams.insertOne({
 teams.insertOne({
   "name": "test team 6"
 })
-
 
 print('🏁 Finished seeding MongoDB');
--- scouting-app.py
+++ scouting-app.py
@@ -5,6 +5,8 @@ from flask_login import LoginManager, current_user, login_required, login_user, 
 from flask_socketio import SocketIO
 from config import CONFIG
 from data import forms
+from data import quals
+from data import pits
 from data import teams
 from data import users
 from utils.log import log
@@ -29,9 +31,9 @@ def get_form_options():
         "teams": list(map(lambda team: team["name"], teams.getAll())),
         "starting_position": [
             "Not There",
-            "Left Side",
-            "Middle",
-            "Coral Station Side"
+            "Coral Side",
+            "Middle of Reef",
+            "Processor Side"
         ],
         "left_start": [
             "No",
@@ -54,9 +56,8 @@ def get_form_options():
             "Not Parked",
             "Parked",
             "Cage Attempted",
-            "On Cage",
-            "Hung with 1 Robot",
-            "Hung with 2 Robots"
+            "Shallow Cage",
+            "Deep Cage"
         ]
     }
 
@@ -80,7 +81,11 @@ def match_data():
 
     sorted_forms = sorted(forms.getAll(), key=itemgetter("created_on"), reverse=True)
 
-    return render_template("match_data.html", forms=sorted_forms, bag=get_bag() )
+    sorted_quals = sorted(quals.getAll(), key=itemgetter("created_on"), reverse=True)
+
+    sorted_pits = sorted(pits.getAll(), key=itemgetter("created_on"), reverse=True)
+
+    return render_template("match_data.html", pits=sorted_pits, quals=sorted_quals, forms=sorted_forms, bag=get_bag() )
 
 
 @app.route("/login", methods=["POST"])
@@ -132,8 +137,30 @@ def new_2025_form():
 
     vm = {
         "match_number": "",
+        "scouter_name": "",
+        "team": "",
         "starting_position": "Not There",
         "left_start": "No",
+        "auto_algae_score": 0,
+        "auto_algae_misses": 0,
+        "auto_processed": 0,
+        "auto_processor_misses": 0,
+        "auto_L1_score": 0,
+        "auto_L2_score": 0,
+        "auto_L3_score": 0,
+        "auto_L4_score": 0,
+        "auto_coral_misses": 0,
+        "teleop_algae_score": 0,
+        "teleop_algae_misses": 0,
+        "teleop_processed": 0,
+        "teleop_processor_misses": 0,
+        "teleop_L1_score": 0,
+        "teleop_L2_score": 0,
+        "teleop_L3_score": 0,
+        "teleop_L4_score": 0,
+        "teleop_coral_misses": 0,
+        "human_score": 0,
+        "human_misses": 0,
         "algae_intake": "Does not pick up Algae",
         "coral_intake": "Does not pick up Coral",
         "start_hang": 0,
@@ -151,23 +178,27 @@ def new_2025_form():
 @login_required
 def save_2025_form():
   form = request.form
-  totalcoralscore = int(form.get("teleop_L1_score")) + int(form.get("teleop_L2_score")) + int(form.get("teleop_L3_score")) + int(form.get("teleop_L4_score"))
-  totalcoralshots = int(totalcoralscore) + int(form.get("teleop_coral_misses"))
-  totalalgaeshots = int(form.get("teleop_algae_score")) + int(form.get("teleop_algae_misses"))
-  totalprocessorshots = int(form.get("teleop_processed")) + int(form.get("teleop_processor_misses"))
-  totalhumanshots = int(form.get("human_score")) + int(form.get("human_misses"))
-  teleop_coral_accuracy = totalcoralscore / totalcoralshots
-  teleop_algae_accuracy = int(form.get("teleop_algae_score")) / totalalgaeshots
-  teleop_processor_accuracy = int(form.get("teleop_processed")) / totalprocessorshots
-  human_accuracy = int(form.get("human_score")) / totalhumanshots
+  totalcoralscore = int(form.get("teleop_L1_score") or 0) + int(form.get("teleop_L2_score") or 0) + int(form.get("teleop_L3_score") or 0) + int(form.get("teleop_L4_score") or 0)
+  totalcoralshots = int(totalcoralscore) + int(form.get("teleop_coral_misses") or 0)
+  totalalgaeshots = int(form.get("teleop_algae_score") or 0) + int(form.get("teleop_algae_misses") or 0)
+  totalprocessorshots = int(form.get("teleop_processed") or 0) + int(form.get("teleop_processor_misses") or 0)
+  totalhumanshots = int(form.get("human_score") or 0) + int(form.get("human_misses") or 0)
+  teleop_coral_accuracy = totalcoralscore / totalcoralshots if totalcoralshots != 0 else 0
+  teleop_algae_accuracy = int(form.get("teleop_algae_score")) / totalalgaeshots if totalalgaeshots != 0 else 0
+  teleop_processor_accuracy = int(form.get("teleop_processed")) / totalprocessorshots if totalprocessorshots != 0 else 0
+  human_accuracy = int(form.get("human_score")) / totalhumanshots if totalhumanshots != 0 else 0
+  climb_speed = int(form.get("start_hang")) - int(form.get("stop_hang"))
   created_on = str(datetime.datetime.now())
   forms.add({
     "team": form.get("team"),
     "match_number": form.get("match_number"),
+    "scouter_name": form.get("scouter_name"),
     "teleop_coral_accuracy": f"{teleop_coral_accuracy:.1%}",
     "teleop_algae_accuracy": f"{teleop_algae_accuracy:.1%}",
     "teleop_processor_accuracy": f"{teleop_processor_accuracy:.1%}",
     "human_accuracy": f"{human_accuracy:.1%}",
+    "algae_intake": form.get("algae_intake"),
+    "coral_intake": form.get("coral_intake"),
     "starting_position": form.get("starting_position"),
     "left_start": form.get("left_start"),
     "auto_algae_score": form.get("auto_algae_score"),
@@ -192,7 +223,7 @@ def save_2025_form():
     "human_misses": form.get("human_misses"),
     "start_hang": form.get("start_hang"),
     "stop_hang": form.get("stop_hang"),
-    "trap_score": form.get("trap_score"),
+    "climb_speed": climb_speed,
     "end_status": form.get("end_status"),
     "comments": form.get("comments"),
     "created_by": current_user.username,
@@ -212,7 +243,10 @@ def load_2025_form(id):
     vm = {
     "team": form.get("team"),
     "match_number": form.get("match_number"),
+    "scouter_name": form.get("scouter_name"),
     "starting_position": form.get("starting_position"),
+    "algae_intake": form.get("algae_intake"),
+    "coral_intake": form.get("coral_intake"),
     "left_start": form.get("left_start"),
     "auto_algae_score": form.get("auto_algae_score"),
     "auto_algae_misses": form.get("auto_algae_misses"),
@@ -236,7 +270,6 @@ def load_2025_form(id):
     "human_misses": form.get("human_misses"),
     "start_hang": form.get("start_hang"),
     "stop_hang": form.get("stop_hang"),
-    "trap_score": form.get("trap_score"),
     "end_status": form.get("end_status"),
     "comments": form.get("comments"),
     "options": get_form_options()
@@ -248,26 +281,29 @@ def load_2025_form(id):
 @login_required
 def update_2025_form(id):
   
-  created_on: str(datetime.datetime.now())
   form = request.form
-  totalcoralscore = int(form.get("teleop_L1_score")) + int(form.get("teleop_L2_score")) + int(form.get("teleop_L3_score")) + int(form.get("teleop_L4_score"))
-  totalcoralshots = int(totalcoralscore) + int(form.get("teleop_coral_misses"))
-  totalalgaeshots = int(form.get("teleop_algae_score")) + int(form.get("teleop_algae_misses"))
-  totalprocessorshots = int(form.get("teleop_processed")) + int(form.get("teleop_processor_misses"))
-  totalhumanshots = int(form.get("human_score")) + int(form.get("human_misses"))
-  teleop_coral_accuracy = totalcoralscore / totalcoralshots
-  teleop_algae_accuracy = int(form.get("teleop_algae_score")) / totalalgaeshots
-  teleop_processor_accuracy = int(form.get("teleop_processed")) + totalprocessorshots
-  human_accuracy = int(form.get("human_score")) / totalhumanshots
-
+  totalcoralscore = int(form.get("teleop_L1_score") or 0) + int(form.get("teleop_L2_score") or 0) + int(form.get("teleop_L3_score") or 0) + int(form.get("teleop_L4_score") or 0)
+  totalcoralshots = int(totalcoralscore) + int(form.get("teleop_coral_misses") or 0)
+  totalalgaeshots = int(form.get("teleop_algae_score") or 0) + int(form.get("teleop_algae_misses") or 0)
+  totalprocessorshots = int(form.get("teleop_processed") or 0) + int(form.get("teleop_processor_misses") or 0)
+  totalhumanshots = int(form.get("human_score") or 0) + int(form.get("human_misses") or 0)
+  teleop_coral_accuracy = totalcoralscore / totalcoralshots if totalcoralshots != 0 else 0
+  teleop_algae_accuracy = int(form.get("teleop_algae_score")) / totalalgaeshots if totalalgaeshots != 0 else 0
+  teleop_processor_accuracy = int(form.get("teleop_processed")) / totalprocessorshots if totalprocessorshots != 0 else 0
+  human_accuracy = int(form.get("human_score")) / totalhumanshots if totalhumanshots != 0 else 0
+  climb_speed = int(form.get("start_hang")) - int(form.get("stop_hang"))
+  created_on = str(datetime.datetime.now())
   forms.update(id, {
     "team": form.get("team"),
     "match_number": form.get("match_number"),
+    "scouter_name": form.get("scouter_name"),
     "teleop_coral_accuracy": f"{teleop_coral_accuracy:.1%}",
     "teleop_algae_accuracy": f"{teleop_algae_accuracy:.1%}",
     "teleop_processor_accuracy": f"{teleop_processor_accuracy:.1%}",
     "human_accuracy": f"{human_accuracy:.1%}",
     "starting_position": form.get("starting_position"),
+    "algae_intake": form.get("algae_intake"),
+    "coral_intake": form.get("coral_intake"),
     "left_start": form.get("left_start"),
     "auto_algae_score": form.get("auto_algae_score"),
     "auto_algae_misses": form.get("auto_algae_misses"),
@@ -291,7 +327,7 @@ def update_2025_form(id):
     "human_misses": form.get("human_misses"),
     "start_hang": form.get("start_hang"),
     "stop_hang": form.get("stop_hang"),
-    "trap_score": form.get("trap_score"),
+    "climb_speed": climb_speed,
     "end_status": form.get("end_status"),
     "comments": form.get("comments"),
     "updated_by": current_user.username,
@@ -299,6 +335,203 @@ def update_2025_form(id):
   })
 
   return redirect("/data")
+
+@app.route("/form/qual/new", methods=['GET'])
+@login_required
+def new_super_scouting_form():
+
+    vm = {
+        "match_number": "",
+        "scouter_name": "",
+        "comments": ""
+    }
+
+
+
+    return render_template("super_scouting_form.html", vm=vm, bag=get_bag())
+
+@app.route("/form/qual/new", methods=["POST"])
+@login_required
+def save_super_scouting_form():
+  form = request.form
+  created_on = str(datetime.datetime.now())
+  quals.add({
+    "team": form.get("team"),
+    "match_number": form.get("match_number"),
+    "scouter_name": form.get("scouter_name"),
+    "comments": form.get("comments"),
+    "created_by": current_user.username,
+    "updated_by": current_user.username,
+    "created_on": created_on,
+    "updated_on": created_on,
+    })
+
+  return redirect("/scouting_dashboard")
+
+@app.route("/form/qual/<id>", methods=['GET'])
+@login_required
+def load_super_scouting_form(id):
+
+    form = quals.get(id)
+
+    vm = {
+    "team": form.get("team"),
+    "match_number": form.get("match_number"),
+    "scouter_name": form.get("scouter_name"),
+    "comments": form.get("comments")
+    }
+
+    return render_template("super_scouting_form.html", vm=vm, bag=get_bag())
+
+@app.route("/form/qual/<id>", methods=["POST"])
+@login_required
+def update_super_scouting_form(id):
+  
+  form = request.form
+  created_on = str(datetime.datetime.now())
+  quals.update(id, {
+    "team": form.get("team"),
+    "match_number": form.get("match_number"),
+    "scouter_name": form.get("scouter_name"),
+    "comments": form.get("comments"),
+    "updated_by": current_user.username,
+    "updated_on": created_on,
+  })
+
+  return redirect("/data")
+
+def get_pit_options():
+    return {
+        "teams": list(map(lambda team: team["name"], teams.getAll())),
+        "base": [
+            "Swerve L1",
+            "Swerve L2",
+            "Swerve L3",
+            "Tank",
+            "Mecanum"
+        ],
+        "coral_intake": [
+            "Does not pick up Coral",
+            "From the Ground",
+            "From the Coral Station",
+            "Both"
+        ],
+        "algae_intake": [
+            "Does not pick up Algae",
+            "From the Ground",
+            "From the Reef",
+            "Both"
+        ]
+    }
+
+@app.route("/form/pit/new", methods=['GET'])
+@login_required
+def new_pit_form():
+
+    vm = {
+        "scouter_name": "",
+        "team": "",
+        "dimensions": "",
+        "weight": "",
+        "algae_intake": "Does not pick up Algae",
+        "coral_intake": "Does not pick up Coral",
+        "auto_best_score": 0,
+        "comments": "",
+        "options": get_pit_options()
+    }
+
+
+
+    return render_template("pit_form.html", vm=vm, bag=get_bag())
+
+@app.route("/form/pit/new", methods=["POST"])
+@login_required
+def save_pit_form():
+  form = request.form
+  created_on = str(datetime.datetime.now())
+  pits.add({
+    "scouter_name": form.get("scouter_name"),
+    "team": form.get("team"),
+    "base": form.get("base"),
+    "dimensions": form.get("dimensions"),
+    "weight": form.get("weight"),
+    "algae_intake": form.get("algae_intake"),
+    "coral_intake": form.get("coral_intake"),
+    "L4": form.get("L4"),
+    "L3": form.get("L3"),
+    "L2": form.get("L2"),
+    "L1": form.get("L1"),
+    "auto_move": form.get("auto_move"),
+    "auto_score": form.get("auto_score"),
+    "auto_best_score": form.get("auto_best_score"),
+    "shallow": form.get("shallow"),
+    "deep": form.get("deep"),
+    "comments": form.get("comments"),
+    "created_by": current_user.username,
+    "updated_by": current_user.username,
+    "created_on": created_on,
+    "updated_on": created_on,
+    })
+
+  return redirect("/scouting_dashboard")
+
+@app.route("/form/2025/<id>", methods=['GET'])
+@login_required
+def load_pit_form(id):
+
+    form = pits.get(id)
+
+    vm = {
+    "scouter_name": form.get("scouter_name"),
+    "team": form.get("team"),
+    "base": form.get("base"),
+    "dimensions": form.get("dimensions"),
+    "weight": form.get("weight"),
+    "algae_intake": form.get("algae_intake"),
+    "coral_intake": form.get("coral_intake"),
+    "L4": form.get("L4"),
+    "L3": form.get("L3"),
+    "L2": form.get("L2"),
+    "L1": form.get("L1"),
+    "auto_move": form.get("auto_move"),
+    "auto_score": form.get("auto_score"),
+    "auto_best_score": form.get("auto_best_score"),
+    "shallow": form.get("shallow"),
+    "deep": form.get("deep"),
+    "comments": form.get("comments"),
+    "options": get_form_options()
+    }
+
+    return render_template("2025_form.html", vm=vm, bag=get_bag())
+
+@app.route("/form/pit/<id>", methods=["POST"])
+@login_required
+def update_pit_form():
+  form = request.form
+  created_on = str(datetime.datetime.now())
+  pits.add({
+    "scouter_name": form.get("scouter_name"),
+    "team": form.get("team"),
+    "base": form.get("base"),
+    "dimensions": form.get("dimensions"),
+    "weight": form.get("weight"),
+    "algae_intake": form.get("algae_intake"),
+    "coral_intake": form.get("coral_intake"),
+    "L4": form.get("L4"),
+    "L3": form.get("L3"),
+    "L2": form.get("L2"),
+    "L1": form.get("L1"),
+    "auto_move": form.get("auto_move"),
+    "auto_score": form.get("auto_score"),
+    "auto_best_score": form.get("auto_best_score"),
+    "shallow": form.get("shallow"),
+    "deep": form.get("deep"),
+    "comments": form.get("comments"),
+    "updated_by": current_user.username,
+    "updated_on": created_on,
+    })
+
+  return redirect("/scouting_dashboard")
 
 
 @app.route("/scouting_dashboard", methods=["GET"])
